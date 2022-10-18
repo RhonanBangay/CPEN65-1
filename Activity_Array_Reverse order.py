d768756b02d58713d4752eede26283b617560e3f
RHONAN RICHMOND D. BANGAY <90811762+RhonanBangay@users.noreply.github.com>
2022-10-18 20:06:12 +0800

    Add files via upload

---
+++ Activity_Array_Reverse order.py
@@ -0,0 +1,10 @@
+#Create a python program that use Array
+#Insert each character of your full name and displays each character of your full name in reverse order
+
+array_name = ["R","H","O","N","A","N","R","I","C","H","M","O","N","D", "D." ,"B","A","N","G","A","Y"]
+original = array_name
+
+print(" Original Array: ",original)
+array_name.reverse()
+
+print("\n Reverse Order: ",array_name)
